af3b6718714b177dc16a5ea5998a74f9c0f60577
yayajojo <fleetinghappines@gmail.com>
2020-01-07 17:59:01 +0800

    witing for content of pop, pattern all finished

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// jshint esversion:6
+ // jshint esversion:6
 // popup window displayed for showing the work experience
 // Get the modal
-// 1. woking-holiday javascript:"woking-holiday_modal", "woking-holiday_btn", "woking-holiday_close"
+// 1. woking-holiday js:"woking-holiday_modal", "woking-holiday_btn", "woking-holiday_close"
 
 let woHoBtn = document.getElementById("woking-holiday_btn");
 let woHoModal = document.getElementById("woking-holiday_modal");
@@ -20,6 +20,9 @@ window.onclick = function(event) {
     woHoModal.style.display = "none";
   }
 };
+
+// 2.graduate js
+
 let gaeaBtn = document.getElementById("gaea_btn");
 let gaeaModal = document.getElementById("gaea_modal");
 let gaeaClose = document.getElementById("gaea_close");
@@ -35,5 +38,61 @@ gaeaClose.onclick = function() {
 window.onclick = function(event) {
   if (event.target == gaeaModal) {
     gaeaModal.style.display = "none";
+  }
+};
+// 3. patent js
+let patentBtn = document.getElementById("patent_btn");
+let patentModal = document.getElementById("patent_modal");
+let patentClose = document.getElementById("patent_close");
+// When the user clicks on the button, open the modal
+patentBtn.onclick = function() {
+  patentModal.style.display = "block";
+};
+// When the user clicks on <span> (x), close the modal
+patentClose.onclick = function() {
+  patentModal.style.display = "none";
+};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == patentModal) {
+    patentModal.style.display = "none";
+  }
+};
+// 4. graduate js
+
+let graduateBtn = document.getElementById("graduate_btn");
+let graduateModal = document.getElementById("graduate_modal");
+let graduateClose = document.getElementById("graduate_close");
+// When the user clicks on the button, open the modal
+graduateBtn.onclick = function() {
+  graduateModal.style.display = "block";
+};
+// When the user clicks on <span> (x), close the modal
+graduateClose.onclick = function() {
+  graduateModal.style.display = "none";
+};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == graduateModal) {
+    graduateModal.style.display = "none";
+  }
+};
+// 5. college js
+
+let collegeBtn = document.getElementById("college_btn");
+let collegeModal = document.getElementById("college_modal");
+let collegeClose = document.getElementById("college_close");
+// When the user clicks on the button, open the modal
+collegeBtn.onclick = function() {
+  collegeModal.style.display = "block";
+};
+// When the user clicks on <span> (x), close the modal
+collegeClose.onclick = function() {
+  collegeModal.style.display = "none";
+};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == collegeModal) {
+    collegeModal.style.display = "none";
   }
 };
